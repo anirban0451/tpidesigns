f4253eef9dfd549b3ef54ee89e8cf607314ce325
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-10-26 06:27:08 -0500

    Developed the function to calculate weight for priors and corresponding parameter values.

---
+++ R/weights_formulate.R
@@ -0,0 +1,42 @@
+weights_formulate = function(w = 1, x, n, a1 = 1, a2= 1, b1 = 1, b2 = 1)
+{
+  if (w > 1)
+  {
+    stop("Weight on informative prior can be at most 1")
+  }
+  else if (w < 0)
+  {
+    stop("Weight on a prior can not be negative")
+  }
+  if (a1 < 0 || b1 < 0 || a2 < 0 || b2 <0)
+  {
+    stop("Beta parameters can not be negative")
+  }
+  if(x > n)
+  {
+    stop("Number of successes for the event (i.e. experiencing DLT 's) must be lower than total number of trials (i.e. patients treated)")
+  }
+  if (w == 1)
+  {
+    param_inform = c(a1 + x, b1 + n - x)
+    param_noninform = c(0, 0)
+  }
+  else if (w == 0)
+  {
+    param_inform = c(0,0)
+    param_noninform = c(a2 + x, b2 + n - x)
+  }
+  else
+  {
+    param_inform = c(a1 + x, n - x + b1)
+    param_noninform = c(a2 + x, n - x + b2)
+    w = (w *( beta(a1 + x, b1 + n - x)/beta(a1, b1)))/
+      (w *( beta(a1 + x, b1 + n - x)/beta(a1, b1)) +
+         (1 - w) *( beta(a2 + x, b2 + n - x)/beta(a2, b2)))
+  }
+  ls = list()
+  ls$weights = c(w, 1 - w)
+  ls$param_inform = param_inform
+  ls$param_noninform = param_noninform
+  return(ls)
+}
