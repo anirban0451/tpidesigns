c8c213558bff5bbe721b2e222415c0fcb58c1537
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-10-27 22:30:24 -0500

    Included Dosing Decisions in the function

--- R/decisiontpi.R
+++ R/decisiontpi.R
@@ -20,9 +20,12 @@ decisiontpi <- function(pt, e1 = 0.05, e2 = 0.05, x, n, eta, design = c("tpi", "
     stop("Number of patients experiencing DLT 's must be less than or equal to total number of patients treated in that cohort.")
   }
 
-  if (w %in% c(0,1) && isTRUE(pbeta(pt, a1 + x, b1 + n -x, lower.tail = FALSE) > eta))
+  #Checking over toxicity of Doses
+  threshold = 0
+  if (w %in% c(0,1))
   {
-    return("DU")
+    threshold = pbeta(pt, a1 + x, b1 + n -x, lower.tail = FALSE)
+    if (threshold >= eta)return("DU")
   }
   else
   {
@@ -32,21 +35,66 @@ decisiontpi <- function(pt, e1 = 0.05, e2 = 0.05, x, n, eta, design = c("tpi", "
     b1 = params$param_inform[2]
     a2 = params$param_noninform[1]
     b2 = params$param_noninform[2]
-    if(isTRUE(w * pbeta(pt, a1 + x, b1 + n - x, lower.tail = FALSE) +
-              (1 - w) * pbeta(pt, a2 + x, b2 + n - x, lower.tail = FALSE)> eta))
+    threshold = w * pbeta(pt, a1 + x, b1 + n - x, lower.tail = FALSE) +
+                         (1 - w) * pbeta(pt, a2 + x, b2 + n - x, lower.tail = FALSE)
+    if(threshold >= eta)
     {
       return("DU")
     }
   }
 
+
   #breaking up the domain in compatible ranges according to Professor Yuan Ji 's paper
   if (design %in% c("tpi", "mtpi"))
   {
-    interval = c(0, pt - e1, pt + e2)
+    interval = c(0, pt - e1, pt + e2 , 1)
+    length_interval = length(interval)
+    upm_array = rep(0, length_interval - 1)
+    for (i  in 1:(length_interval - 1))
+    {
+      upm_array[i] = UPM(w = w, a1 = a1, a2 = a2, b1 = b1, b2 = b2, a = interval[i], b = interval[i + 1])
+    }
+    max_upm = max(upm_array)
+    location = which.max(upm_array)
+    if(location == 1)
+    {
+      return("E")
+    }
+    else if (location == 2)
+    {
+      return("S")
+    }
+    else
+    {
+      return("D")
+    }
   }
   else
   {
     breaks_lower = floor((pt - e1) / 0.1)
-  }
+    breaks_upper = floor((1 - pt - e2)  / 0.1)
+    interval = c(0, pt - e1 - 0.1 * (breaks_lower : 0) , pt + e2 + 0.1 * (0 : breaks_upper) , 1)
+    length_interval = length(interval)
+    upm_array = rep(0, length_interval - 1)
 
+    #Dose Finding Decision
+    for (i  in 1:(length_interval - 1))
+    {
+      upm_array[i] = UPM(w = w, a1 = a1, a2 = a2, b1 = b1, b2 = b2, a = interval[i], b = interval[i + 1])
+    }
+    max_upm = max(upm_array)
+    location = which.max(upm_array)
+    if(interval[location] < (pt - e1))
+    {
+      return("E")
+    }
+    else if (interval[location] == (pt - e1))
+    {
+      return("S")
+    }
+    else
+    {
+      return("D")
+    }
+  }
 }
