63808544ff0e506d1f90ffc1ddedef4c2ab36231
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-11-02 00:57:30 -0500

    Creation of tpitable() function to create a table of patient thresholds for dosing

---
+++ R/tpitable.R
@@ -0,0 +1,15 @@
+tpitable = function(nmax, cohort, w, a1 = NULL, b1 = NULL, a2 = NULL, b2 = NULL)
+{
+  if(isTRUE(namx < 3))
+  {
+    if(isTRUE(nmax <= 0))
+    {
+      stop("Number of patients must be positive and at least 3")
+    }
+    else
+    {
+      stop("Number of patients must be atleast 3")
+    }
+  }
+
+}
