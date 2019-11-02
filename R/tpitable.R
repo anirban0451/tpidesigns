db53330d10f4eff36472ae8e587924b41848d8f2
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-11-02 18:49:39 -0500

    Completion of tpitable function for primary objective, to see overall dose structure

--- R/tpitable.R
+++ R/tpitable.R
@@ -1,6 +1,6 @@
-tpitable = function(nmax, cohort, w, a1 = NULL, b1 = NULL, a2 = NULL, b2 = NULL)
+tpitable = function(nmax, design, pt, e1 = 0.05, e2 = 0.05, eta, w, a1 = NULL, b1 = NULL, a2 = NULL, b2 = NULL)
 {
-  if(isTRUE(namx < 3))
+  if(isTRUE(nmax < 3))
   {
     if(isTRUE(nmax <= 0))
     {
@@ -11,5 +11,31 @@ tpitable = function(nmax, cohort, w, a1 = NULL, b1 = NULL, a2 = NULL, b2 = NULL)
       stop("Number of patients must be atleast 3")
     }
   }
+  table = matrix(0, nrow = 4, ncol = nmax - 2)
+  for(i in 3:nmax)
+  {
+    decisions = array(dim = 1)
+    for(dlt in 0:i)
+    {
+      decisions[dlt + 1] = decisiontpi(pt, e1, e2, x = dlt, n = i, eta, design, w, a1, b1, a2, b2)
+      if(isTRUE(decisions[dlt + 1] == "DU"))
+      {
+        table[4 , i - 2] = dlt
+        break
+      }
+    }
 
+    table[1 , i - 2] = ifelse(any(decisions == "E"), max(which(decisions == "E")) - 1, NA)
+
+    table[2 , i - 2] = ifelse(any(decisions == "S"), max(which(decisions == "S")) - 1, NA)
+
+    table[3 , i - 2] = ifelse(any(decisions == "D"), max(which(decisions == "D")) - 1, NA)
+  }
+  rownames_table = c("Maximum no. of DLT's allowed to Escalate from the current Dosage",
+                     "Maximum no. of DLT's allowed to Stay at the current Dosage",
+                     "Maximum no. of DLT's allowed to De-escalate to a lower Dosage",
+                     "Minimum number of DLT's to declare current dosage as Unacceptably toxic")
+  data.frame(table, row.names = rownames_table) -> tableframe
+  names(tableframe) <- paste("n = ", 3:nmax)
+  return(tableframe)
 }
