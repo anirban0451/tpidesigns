0d20c4df322ca9a28c02f576df03b815b9aa9992
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-11-06 01:50:07 -0600

    Inclusion of upmplot function that requires  ggplot package in R

--- R/upm.R
+++ R/upm.R
@@ -96,3 +96,129 @@ UPM <- function(w, a = 0, b = 1, a1 = NULL, b1 = NULL, a2 = NULL, b2 = NULL)
 
   return(val)
 }
+
+
+
+require(ggplot2)
+upmplot <- function(x , n , pt, e1 = 0.05, e2 = 0.05, design = c("mtpi", "mmtpi"), w, a1, b1, a2, b2)
+{
+  if (isTRUE(w > 1))
+  {
+    stop("Weight on informative prior can be at most 1")
+  }
+  else if (isTRUE(w < 0))
+  {
+    stop("Weight on a prior can not be negative")
+  }
+
+  #Checking the eligibility of the parameters
+
+  if (isTRUE(sum(c(a1, b1, a2, b2) <= 0) > 0))
+  {
+    stop("Beta parameters must be non-negative")
+  }
+
+  #Checking the number of events happened is less than total number of trials
+
+  if (isTRUE(n < 1))
+  {
+    stop("The trial size must be at least 1")
+  }
+
+  if(isTRUE(x > n))
+  {
+    stop("Number of successes for the event (i.e. experiencing DLT 's) must be lower than total number of trials (i.e. patients treated)")
+  }
+
+  #Checking feasibility condition of prior parameters
+  a1_null = is.null(a1)
+  b1_null = is.null(b1)
+  a2_null = is.null(a2)
+  b2_null = is.null(b2)
+  total_null = a1_null + b1_null + a2_null + b2_null
+
+  if (isTRUE(total_null == 4))
+  {
+    stop("Please input a1, a2, b1, b2 properly. ")
+  }
+
+  if(w %in% c(0, 1))
+  {
+    if (isTRUE(total_null == 2))
+    {
+      if(isTRUE((a2_null + b2_null) == 1))
+      {
+        stop("Please input either both a1 and b1, or both a2 and b2, (ai,bi) is the pair of parameters. For Uniform Distribution, either put a1 = 1, b1 = 1, or put, a2 = 1 and b2 = 1")
+      }
+      else if (isTRUE((a2_null + b2_null) == 0))
+      {
+        a1 = a2
+        b1 = b2
+        warning("You should put the parameter values for a1 and b1 instead of a2 and b2")
+      }
+    }
+    else if (total_null %in% c(1,3))
+    {
+      stop("Please input a1, b1, a2, b2 properly, (ai,bi) is the pair of parameters. For Uniform Distribution, either put a1 = 1, b1 = 1, or put, a2 = 1 and b2 = 1")
+    }
+    else if (isTRUE(total_null == 0))
+    {
+      warning("Check inputs for prior parameters, taking a1 and b1 as original parameters")
+    }
+  }
+  else
+  {
+    if (isTRUE(total_null > 0) )
+    {
+      stop("Please input model parameters  for both priors properly")
+    }
+  }
+
+  if (design == "mtpi")
+  {
+    interval = c(0, pt - e1, pt + e2, 1)
+    length_interval = length(interval)
+  }
+  else if (design %in% c("mtpi", "mmtpi"))
+  {
+    breaks_lower = floor((pt - e1) / 0.1)
+    breaks_upper = floor((1 - pt - e2)  / 0.1)
+    interval = c(0, pt - e1 - 0.1 * (breaks_lower : 0) , pt + e2 + 0.1 * (0 : breaks_upper) , 1)
+    length_interval = length(interval)
+  }
+  else
+  {
+    stop("Please input one input among the designs: mtpi, mmtpi")
+  }
+  params = weights_formulate(w = w, x = x, n = n, a1 = a1, b1 = b1, a2 = a2, b2 = b2)
+  w = params$weight
+  a1 = params$param_inform[1]
+  b1 = params$param_inform[2]
+  a2 = params$param_noninform[1]
+  b2 = params$param_noninform[2]
+  upm_array= rep(0, length_interval - 1)
+  for(i in 1: (length_interval - 1))
+  {
+    upm_array[i] = UPM(w = w, a = interval [i], b = interval [i + 1], a1 = a1, b1 = b1, a2 = a2, b2 = b2)
+  }
+  if (w %in% c(0,1))
+  {
+    plotupm = ggplot(data.frame(x=seq(0.01,1,0.01)), aes(x)) +
+      stat_function(fun=function(x) dbeta(x, shape1 = a1, shape2 = b1))
+  }
+  else
+  {
+    plotupm = ggplot(data.frame(x=seq(0.01,1,0.01)), aes(x)) +
+      stat_function(fun=function(x) w * dbeta(x, shape1 = a1, shape2 = b1) + (1 - w) * dbeta(x, shape1 = a2, shape2 = b2))
+  }
+  plotupm_addY <- plotupm + geom_vline(xintercept = interval, linetype="dashed", color = "steelblue", size = 0.7)
+
+  segment_data = data.frame(x = interval[-length_interval], y = upm_array, xend = interval[-1],yend = upm_array)
+  plotupm_addsegments <- plotupm_addY +
+    geom_segment(data = segment_data, mapping = aes(x = x, xend = xend, y = y, yend = yend))
+
+  plotupm_addfootnote <- plotupm_addsegments +
+    labs(title = " Plotting of UPM values and Posterior DLT distribution", x = "DLT Rate", y = "Unit Probability Mass (UPM)",
+         caption = "The Dashed lines represent the intervals and the Horizontal lines represent the UPM")
+  return(plotupm_addfootnote)
+}
