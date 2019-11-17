b601bdca0853f7e8b20938301f00114675125117
Anirban Chakraborty <anirbanchakraborty@tamu.edu>
2019-11-16 19:48:36 -0600

    Initialisation of testing for weights_formulate() function

--- tests/testthat/test-weights_formulate.R
+++ tests/testthat/test-weights_formulate.R
@@ -1,3 +1,4 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("Error Check", {
+  testthat::expect_error(weights_formulate(w = 0.5, x = 5, n = 10, a1 = NULL, b1= 1, a2 = 2, b2 = 4),
+                        "Please input model parameters  for both priors properly")
 })
